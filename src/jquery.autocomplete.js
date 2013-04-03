2ebaf5e4e5fe979d001b628aa95add07735a7f0e
arjeousski <arjeousski@gmail.com>
2013-04-02 20:02:08 -0400

    Merge pull request #71 from oluckyman/master
    Fix of the "beforeUseConverter" and "getValue" methods signatures

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -1129,15 +1129,13 @@ ISSUES:
 
     /**
      * Convert string before use
-     * @param s
-     * @param a
-     * @param b
+     * @param {String} s
      */
-    $.Autocompleter.prototype.beforeUseConverter = function(s, a, b) {
-        s = this.getValue();
+    $.Autocompleter.prototype.beforeUseConverter = function(s) {
+        s = this.getValue(s);
         var converter = this.options.beforeUseConverter;
         if ($.isFunction(converter)) {
-            s = converter(s, a, b);
+            s = converter(s);
         }
         return s;
     };
@@ -1606,13 +1604,12 @@ ISSUES:
      * Get the value currently being autocompleted
      * @param {String} value
      */
-    $.Autocompleter.prototype.getValue = function() {
-        var val = this.dom.$elem.val();
+    $.Autocompleter.prototype.getValue = function(value) {
         if ( this.options.useDelimiter ) {
             var d = this.getDelimiterOffsets();
-            return val.substring(d.start, d.end).trim();
+            return value.substring(d.start, d.end).trim();
         } else {
-            return val;
+            return value;
         }
     };
 
